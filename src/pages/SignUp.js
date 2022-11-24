20f4fa525891fe32e8f8e058f6024c43fa215cbd
Nabeel M <nabeelm25698@gmail.com>
2022-11-24 21:22:34 +0530

    Firestore_implemented

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import {db} from '../firebase.config.js';
+import {setDoc, doc, serverTimestamp} from 'firebase/firestore';
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 const SignUp = () => {
+    const navigate = useNavigate();
     const [showPassword , setShowPassword] = useState(false);
     const [formData , setFormData] = useState({
         name :'',
@@ -16,13 +20,38 @@ const SignUp = () => {
             [e.target.id]:e.target.value}
         })
     }
-    console.log(formData)
+    
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            const userCredential = await createUserWithEmailAndPassword(
+                auth ,
+                email, 
+                password
+                )
+                const user = userCredential.user
+                updateProfile(auth.currentUser,{
+                    displayName:name
+                })
+                const formDataCopy = {...formData}
+                delete formDataCopy.password;
+                formDataCopy.timestamp = serverTimestamp();
+                
+                await setDoc(doc(db,'users',user.uid),formDataCopy);
+
+                navigate('/')
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
     return (
         <div className="page-container">
             <header>
                 <p>Welcome Back!</p>
             </header>
-            <form>
+            <form onSubmit={onSubmit}>
             <input 
                     type='text'
                     placeholder="Name" 
@@ -56,7 +85,7 @@ const SignUp = () => {
                 </div>
                 <div className="signUpBar">
                     <p className="signUpText">Sign Up</p>
-                    <button className="signUpButton">
+                    <button className="signUpButton" type="submit">
                         <ArrowRightIcon fill="#ffffff" width='34px'/>
                     </button>
                 </div>
